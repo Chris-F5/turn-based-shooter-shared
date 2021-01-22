e8285b383e8d99fe540b9bbe122796958ebca969
Chris-F5 <christopher-d-lang@outlook.com>
2021-01-22 18:57:19 +0000

    Change names to not conflict with web libs

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Request {
+pub struct TestRequest {
     pub name: String,
 }
 
-impl Request {
-    pub fn new(name: String) -> Request {
-        Request { name }
+impl TestRequest {
+    pub fn new(name: String) -> TestRequest {
+        TestRequest { name }
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Response {
+pub struct TestResponse {
     pub number: u32,
     pub message: String,
 }
 
-impl Response {
-    pub fn new(number: u32, message: String) -> Response {
-        Response { number, message }
+impl TestResponse {
+    pub fn new(number: u32, message: String) -> TestResponse {
+        TestResponse { number, message }
     }
 }
